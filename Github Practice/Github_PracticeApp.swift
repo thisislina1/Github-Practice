e37e985caaf9e1ae274457b1795edbe992c9ba0b
Lina <simplyher.lina@gmail.com>
2023-06-12 13:26:41 -0400

    Initial Commit

---
+++ Github Practice/Github_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_PracticeApp.swift
+//  Github Practice
+//
+//  Created by Lina Lin on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
